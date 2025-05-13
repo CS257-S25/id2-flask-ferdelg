60109538be99919dbc377c5f0ff57f225602ee79
ferdelgranado <delgranadom@carleton.edu>
2025-05-12 23:17:21 -0500

    testing code

---
+++ Tests/test_app.py
@@ -0,0 +1,68 @@
+"""
+test_app.py
+
+This module contains unit tests for the StreamSearch web app.
+It tests the functionality of the homepage and actor route.
+"""
+
+import unittest
+import app
+
+class TestHomepage(unittest.TestCase):
+    """Test the homepage route to ensure it contains the correct content."""
+
+    def test_homepage(self):
+        """Check if the homepage function returns the correct content."""
+        expected_homepage = (
+            """
+            <h1>Welcome to the StreamSearch Homepage! <3</h1></br></br>
+            StreamSearch can help you find movies and TV shows from a specific actor!</br></br>
+            <b>To Use enter the following:</b></br>
+            * <b>[URL]/actor/name</b>: Enter the name of an actor to find movies or shows they appear in. </br>
+            <b>Try this example:</b></br>
+            [URL]/actor/Emma Stone</br>
+            """
+        )
+        self.assertEqual(app.homepage(), expected_homepage)
+
+
+class TestFilterFunctions(unittest.TestCase):
+    """Test class for the Filter class and its methods."""
+    def test_actor_filter(self):
+        """Check if filtering by actor includes the correct titles."""
+        self.assertIn(
+            "The Grand Seduction",
+            app.search_by_actor("Brendan Gleeson")
+            )
+
+    def test_actor_filter_lowercase(self):
+        """Check if filtering with all fields in lowercase includes only correct titles."""
+        self.assertEqual(
+            app.search_by_actor("brendan-gleeson"),
+            "The Grand Seduction"
+        )
+
+    def test_actor_filter_mixedcase(self):
+        """Check if filtering with all fields in mixedcase includes only correct titles."""
+        self.assertEqual(
+            app.search_by_actor("bReNdan%20gleEsOn"),
+            "The Grand Seduction"
+        )
+
+    def test_actor_filter_uppercase(self):
+        """Check if filtering with all fields in uppercase includes only correct titles."""
+        self.assertEqual(
+            app.search_by_actor("BRENDAN_GLEESON"),
+            "The Grand Seduction"
+        )
+
+    def test_nonexistent_actor(self):
+        """Check if filtering by a nonexistent actor results in an empty string."""
+        self.assertEqual(app.search_with_filters("John-Yu", "-", "-"), "")
+
+
+
+
+
+if __name__ == "__main__":
+    unittest.main()
--- app.py
+++ app.py
@@ -1,1 +1,44 @@
-'''Replace me with your flask app'''
+"""
+ID2 flask app.
+"""
+
+from flask import Flask
+from ProductionCode.filtering import Filter, FilteredData
+from ProductionCode.data import Data
+from ProductionCode.formatting import make_list_web_displayable
+
+app = Flask(__name__)
+dataset = Data()
+filtering = Filter(dataset)
+
+@app.route('/')
+def homepage():
+    """
+    Determines the text on the homepage of the website. 
+    Displays detailed instructions regarding the usage of the application.
+    """
+    return (
+        """
+        <h1>Welcome to the StreamSearch Homepage! <3</h1></br></br>
+        StreamSearch can help you find movies and TV shows from a specific actor!</br></br>
+        <b>To Use enter the following:</b></br>
+        * <b>[URL]/actor/name</b>: Enter the name of an actor to find movies or shows they appear in. </br>
+        <b>Try this example:</b></br>
+        [URL]/actor/Emma Stone</br>
+        """
+    )
+
+
+@app.route('/actor/<name>', strict_slashes=False)
+def search_by_actor(name):
+    """
+    Route that returns movie title of a specific actor.
+    """
+    print("Name:", name)
+    results = filtering.filter_by_actor(name)
+    print("Results:", results.get_data())
+    print("Filtered titles:", results.get_titles_list())
+    print("Web Displayabales", make_list_web_displayable(results.get_titles_list()))
+    return make_list_web_displayable(results.get_titles_list())
+if __name__ == "__main__":
+    app.run()
